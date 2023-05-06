b93beb0d2f71c906e64cf06a2360350dad2b6a74
Evgeni <ewgeny11@mail.ru>
2023-05-06 13:37:55 +0300

    added comment

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,5 +1,5 @@
 public class BonusMilesService {
-    public int calculate(int cost) { //int cost - параметры
+    public int calculate(int cost) { //int cost - параметры, которые принимают значение из Main.
         int result = cost / 20;
         return result;
     }
